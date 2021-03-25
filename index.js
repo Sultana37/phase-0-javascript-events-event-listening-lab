a786e3483f6f8aa95b6a03dd00b6f287731b0e46
Esrat <imranzaman.318@gmail.com>
2021-03-25 17:37:14 -0400

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById('input');
 function addingEventListener() {
+    input.addEventListener('click',  
+    function clickAlert() {
+        alert('I was clicked!');
+  });    
 }
+
+ input.addEventListener('click', clickAlert);
+ //addingEventListener();
